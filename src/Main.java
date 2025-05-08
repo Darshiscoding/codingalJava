c25833fa5f44f6a380ca7cf965bda585d9a2079c
darsh <dsinghoct@gmail.com>
2025-05-08 17:10:40 +0530

    java intro

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        print("hello")
+        System.out.println("akash");
+//        sout
+        System.out.println("darsh");
+        int x = 20;//int
+        float y = 35.9887f;//float
+        double z = 3.141414141414141; //  double
+        String name = "akash"; //string
+        boolean isDarshFromIndia = true;//boolean
+
+        System.out.println("value of x "+x);
+        System.out.println("Value of Y:"+y);
+        System.out.println("Value of Z:"+z);
+        System.out.println(isDarshFromIndia);
+
+//        x = "akash"
+
+
+    }
+}
