e1f8b0befa6455f83226714a4300ca7b2dcda02e
darsh <dsinghoct@gmail.com>
2025-05-15 17:16:30 +0530

    Operators

--- src/Operators.java
+++ src/Operators.java
@@ -1,2 +1,27 @@
 public class Operators {
+    public static void main(String[] args) {
+        int x = 10;
+        x++; //post increment operator
+        System.out.println(x);//11
+        ++x;
+        System.out.println(x);//12
+
+        --x;
+        System.out.println(x);//11
+        x--;
+        System.out.println(x);//10
+        System.out.println("-----");
+        System.out.println(x++);//10
+        System.out.println(x);//11
+        System.out.println(x++);//11
+        System.out.println(x);//12
+        System.out.println(x--);//12
+        System.out.println(x);//11
+
+        System.out.println(x++);//11--->12
+        System.out.println(--x);//11
+        System.out.println(--x);//10
+        System.out.println(x--);//10
+        System.out.println(++x);//10
+    }
 }
