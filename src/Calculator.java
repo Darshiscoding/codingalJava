4561e9644ba9ccbce32728110c371ea0f4f863d7
darsh <dsinghoct@gmail.com>
2025-05-15 17:02:57 +0530

    calculator created

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,14 @@
-
 public class Calculator {
-
     public static void main(String[] args) {
-//        +,-,*,/,%
+      double x = 20.1234567123489010;
+      float y = 21.7f;
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("CALCULATOR");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("Product:"+x*y);
+        System.out.println("Sum:"+(x+y));
+        System.out.println("division:"+x/y);
+        System.out.println("remainder:"+x%y);
+        System.out.println("---------------------------------------------------------------------------");
     }
 }
