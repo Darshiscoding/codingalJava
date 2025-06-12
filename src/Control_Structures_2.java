afab8c38edc2a9d858f5c658988ad8cf5e83ac4a
darsh <dsinghoct@gmail.com>
2025-06-12 17:14:05 +0530

    Loop

---
+++ src/Control_Structures_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Control_Structures_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Is it sunny today?");
+        Boolean weather_q = sc.nextBoolean();
+
+        if (weather_q = true){
+
+    }
+}
+}
---
+++ src/Loop.java
@@ -0,0 +1,8 @@
+public class Loop {
+    public static void main(String[] args) {
+        int x = 1;
+        for (int i=1; i<=10; i=i+1){
+            System.out.println(12+"X"+i+"="+(i*12));;
+        }
+    }
+}
