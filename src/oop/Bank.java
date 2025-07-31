2bd85fbe7900127af2f2d540ce5eea946001d7b1
darsh <dsinghoct@gmail.com>
2025-07-31 16:57:31 +0530

    Bank Details

--- src/oop/Bank.java
+++ src/oop/Bank.java
@@ -5,10 +5,10 @@ public class Bank {
     int Balance;
     String AccountNumber;
 
-    Bank(String nameValue, int balance, String accountNumber) {
+    Bank(String nameValue, int balanceValue, String accountNumberValue) {
         name = nameValue;
-        Balance = balance;
-        AccountNumber = accountNumber;
+        Balance = balanceValue;
+        AccountNumber = accountNumberValue;
 
     }
     void Bank_details(){
@@ -17,12 +17,27 @@ public class Bank {
         System.out.println("Account number: "+AccountNumber);
     }
 
+    void deposit(int amount){
+        Balance = Balance + amount;
+    }
+    void withdrawal(int amount){
+        Balance = Balance - amount;
+    }
+
     public static void main(String[] args) {
         Bank darsh = new Bank("Darsh",3000000,"5024098201");
         Bank tejasva = new Bank("Tejasva",300000,"7370987113");
 
         darsh.Bank_details();
         System.out.println("--------------------------------------------------------------");
+        tejasva.Bank_details();
+        darsh.deposit(30000);
+        darsh.Bank_details();
+        tejasva.deposit(100000);
+        tejasva.Bank_details();
+        darsh.withdrawal(30000);
+        darsh.Bank_details();
+        tejasva.withdrawal(100000);
         tejasva.Bank_details();
     }
 }
